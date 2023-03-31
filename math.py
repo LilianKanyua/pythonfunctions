bd495e87ca0248d7ac94f1f15eea6559b0e223d2
LilianKanyua <liliankanyua@gmail.com>
2023-03-31 20:11:18 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtraction(c,d):
+    answer=c-d
+    return answer
+
+def multiplication(e,f):
+    answer=e*f
+    return answer
+
+def division(g,h):
+    answer=g/h
+    return answer
+
+def remainder(i,j):
+    answer=i%j
+    return answer
---
+++ students.py
@@ -0,0 +1,3 @@
+def year_name(name,age):
+    year=2023-age
+    print(f"Hello {name} you were born in {age})
