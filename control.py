618ed825f91114023f8dfcdada3cfe600fcd7223
your_username <kanyualilianm.com>
2023-07-05 23:26:56 +0300

    commit

--- control.py
+++ control.py
@@ -1,19 +1,19 @@
 def even_numbers(x=range(10)):
     for i in x:
-        if(i%2==0):
+        if i%2==0:
             print(i)
 
 
 def odd_numbers(x=range(10)):
     for i in x:
-        if(i%2!=0):
+        if i%2!=0:
             print(f"{i} is divisible by five")
 
 
 def divisible_by_five():
     x=range(30)
     for i in x:
-        if(i%5==0):
+        if i%5==0:
             print(i)
         else:
                 print(f" {i} not divisible by 5")
@@ -22,7 +22,7 @@ def divisible_by_five():
 def multiple_comparison():
     x=range(30)
     for i in x:
-        if(i%2==0):
+        if i%2==0:
             print(f"{i} is divisible by 2")
         elif(i%3==0):
             print(f"{i} is divisible by 3")
@@ -35,7 +35,7 @@ def multiple_comparison():
 def divisible_by_two_and_three():
     x=range(30)
     for i in x:
-        if(i%2==0 and i%3==0):
+        if i%2==0 and i%3==0:
             print(f"{i} is divisible by 2 and 3")
         elif(i%2==0 or i%3==0):
             print(f"{i} is divisible by either 2 or 3")
@@ -45,27 +45,27 @@ def divisible_by_two_and_three():
 
 def another():
     x=1
-    while(x<10):
+    while x<10:
         print("Hello Akirachix")
-        if(x==5):
+        if x==5:
             break
             x+=1
 
 def break_statement():
     x=1
-    while(x<10):
+    while x<10:
         x+1
         print("Hello AkiraChix")
-        if(x==5):
+        if x==5:
             break 
         
 
 def continue_statement():
     x=1
    
-    while(x<10):
+    while x<10:
         x+=1
-        if(x%2!=0):
+        if x%2!=0:
             continue
         print(x)
 
